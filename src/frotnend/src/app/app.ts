4b7edc745db0c7c0b25aeb285c8bd57f6ddefae3
TofTo23 <168311004+TofTo23@users.noreply.github.com>
2025-10-04 18:56:48 +0200

    voice lector added

--- src/frotnend/src/app/app.ts
+++ src/frotnend/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { Navbar } from './navbar/navbar';
 import { RouterOutlet } from '@angular/router';
+import { Lektor } from './lektor/lektor';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, RouterOutlet],
+  imports: [Navbar, RouterOutlet, Lektor],
   templateUrl: './app.html',
 })
 export class App {
   protected title = 'frotnend';
+  textToRead: string = "Wśród nocnej ciszy głos się rozchodzi, wstańcie lehiści. Arkaaaaaa nadchodzi.";
 }
