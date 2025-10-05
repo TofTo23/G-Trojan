445859b345194eca3f0d0ce99101c09a50abbe38
TofTo23 <168311004+TofTo23@users.noreply.github.com>
2025-10-05 02:26:58 +0200

    blogi

--- src/frotnend/src/app/app.routes.ts
+++ src/frotnend/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { PlacesList } from './places-list/places-list';
+import { BlogList } from './blog-list/blog-list';
 
 export const routes: Routes = [
   {
     path: 'places',
     component: PlacesList,
+  },
+  {
+    path: 'blogs',
+    component: BlogList,
   },
   {
     path: '',
--- src/frotnend/src/app/navbar/navbar.ts
+++ src/frotnend/src/app/navbar/navbar.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { LogInButton } from '../log-in-button/log-in-button';
+import { BlogButton } from '../blog-button/blog-button';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, LogInButton],
+  imports: [RouterLink, LogInButton, BlogButton],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
